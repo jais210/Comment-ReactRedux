b7b49a6bb412a32159918ae67fb16a9f71b42e7c
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-25 15:06:39 -0500

    comenzando

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+import { Provider } from 'redux-zero/react';
+import store from './store';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const Index = () => (
+   <Provider store={store}>
+      <HashRouter>
+         <Switch>
+            <Route path="/" component={App} />
+         </Switch>
+      </HashRouter>
+   </Provider>
+);
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+registerServiceWorker();
